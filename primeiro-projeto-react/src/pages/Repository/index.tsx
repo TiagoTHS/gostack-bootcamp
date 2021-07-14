0fab4ba5c74916955de1212c72ba595a192bf33e
Tiago Souza <tiago.souza9935@gmail.com>
2021-07-13 21:15:35 -0300

    Estruturando e estilizando a Dashboard

---
+++ primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+type RepositoryParams = {
+  repository: string;
+};
+
+const Repository: React.FC = () => {
+  const params = useParams<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/61852650?v=4"
+            alt="ASCII"
+          />
+          <div>
+            <strong>TiagoTHS/nlw-together</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>Teste</strong>
+            <p>Teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
+};
+
+export default Repository;
